4459b0cd934065c37df266988a32a5957a47d215
AnanditaRian19 <rizkianandita6@gmail.com>
2020-12-27 23:56:03 +0700

    update

--- Kotlin_GENERIC/src/main/kotlin/com/learn/kotlin/generic/app/Covariant.kt
+++ Kotlin_GENERIC/src/main/kotlin/com/learn/kotlin/generic/app/Covariant.kt
@@ -3,4 +3,15 @@ package com.learn.kotlin.generic.app
 /**
  * @author Rizki Rian Anandita
  */
-    
+class Covariant<out T>(val data: T) {
+    fun data(): T {
+        return data
+    }
+}
+
+fun main() {
+    val covariantString = Covariant<String>("Rizki")
+    val covariantAny: Covariant<Any> = covariantString
+
+    covariantAny.data()
+}
