77106e3f255b125e7d3f1fd1319e28670de09ede
AnanditaRian19 <rizkianandita6@gmail.com>
2020-12-29 23:41:35 +0700

    Update GenericConstraint

--- Kotlin_GENERIC/src/main/kotlin/com/learn/kotlin/generic/app/GenericConstraint.kt
+++ Kotlin_GENERIC/src/main/kotlin/com/learn/kotlin/generic/app/GenericConstraint.kt
@@ -3,17 +3,27 @@ package com.learn.kotlin.generic.app
 /**
  * @author Rizki Rian Anandita
  */
+
+interface CanSayHello {
+    fun sayHello(name: String)
+}
+
 open class Employee
 
 class Manager : Employee()
 
-class VicePresident : Employee()
+class VicePresident : Employee(), CanSayHello {
+    override fun sayHello(name: String) {
+        println("Hello $name, I'm Vice President")
+    }
 
-class Company<T : Any>(val employee: T)
+}
+
+class Company<T>(val employee: T) where T : Employee, T : CanSayHello
 
 fun main() {
-    val data1 = Company(Employee())
-    val data2 = Company(Manager())
+//    val data1 = Company(Employee()) // Error CanSayHello
+//    val data2 = Company(Manager()) // Error CanSayHello
     val data3 = Company(VicePresident())
-    val data4 = Company("String")
+//    val data4 = Company("String") // Error not employee
 }
