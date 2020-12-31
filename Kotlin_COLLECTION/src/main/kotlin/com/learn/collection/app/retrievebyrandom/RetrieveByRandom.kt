11162f243300ea5aa5de12693e415f8d05f89f8a
AnanditaRian19 <rizkianandita6@gmail.com>
2020-12-31 23:02:47 +0700

    create RetrieveByRandom

---
+++ Kotlin_COLLECTION/src/main/kotlin/com/learn/collection/app/retrievebyrandom/RetrieveByRandom.kt
@@ -0,0 +1,14 @@
+package com.learn.collection.app.retrievebyrandom
+
+/**
+ * @author Rizki Rian Anandita
+ */
+fun main() {
+    val range = (1..100).toList()
+
+    println(range.random())
+    println(range.random())
+    println(range.random())
+    println(range.random())
+    println(range.random())
+}
