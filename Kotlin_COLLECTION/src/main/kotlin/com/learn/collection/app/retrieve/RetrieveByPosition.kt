7f0e1ce954d35619f2c72cdcc8334a1d05837f6b
AnanditaRian19 <rizkianandita6@gmail.com>
2020-12-31 22:48:50 +0700

    create RetrieveByPosition

---
+++ Kotlin_COLLECTION/src/main/kotlin/com/learn/collection/app/retrieve/RetrieveByPosition.kt
@@ -0,0 +1,16 @@
+package com.learn.collection.app.retrieve
+
+/**
+ * @author Rizki Rian Anandita
+ */
+fun main() {
+    val set = setOf("Rizki", "Rian", "Anandita")
+
+    println(set.elementAt(0))
+    println(set.first())
+    println(set.last())
+    println(set.elementAtOrNull(2))
+    println(set.elementAtOrNull(10))
+    println(set.elementAtOrElse(2) { "Tidak Ada" })
+    println(set.elementAtOrElse(10) { "Tidak Ada" })
+}
