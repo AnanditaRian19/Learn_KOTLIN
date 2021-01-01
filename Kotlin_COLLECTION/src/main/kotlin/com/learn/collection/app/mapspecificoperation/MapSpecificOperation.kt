5f18b061a80aea5c20225fe4734259d94d18926a
AnanditaRian19 <rizkianandita6@gmail.com>
2021-01-01 22:15:24 +0700

    Update MapSpecificOperation

--- Kotlin_COLLECTION/src/main/kotlin/com/learn/collection/app/mapspecificoperation/MapSpecificOperation.kt
+++ Kotlin_COLLECTION/src/main/kotlin/com/learn/collection/app/mapspecificoperation/MapSpecificOperation.kt
@@ -4,5 +4,12 @@ package com.learn.collection.app.mapspecificoperation
  * @author Rizki Rian Anandita
  */
 fun main() {
+    val map = mapOf("a" to "Rizki", "b" to "Rian", "c" to "Anandita")
 
+//    println(map.getValue("Tidak Ada")) // Exception
+    println(map.getValue("a"))
+    println(map.getOrElse("Tidak Ada") { "Ups" })
+    println(map.filter { entry -> entry.value.length > 5 })
+    println(map.filterKeys { it != "b" })
+    println(map.filterValues { it.length > 5 })
 }
