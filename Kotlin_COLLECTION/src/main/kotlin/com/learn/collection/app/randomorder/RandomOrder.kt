302b16c896007a464d7e6029ad9622aa58ac7615
AnanditaRian19 <rizkianandita6@gmail.com>
2021-01-01 21:16:09 +0700

    create randomOrder

---
+++ Kotlin_COLLECTION/src/main/kotlin/com/learn/collection/app/randomorder/RandomOrder.kt
@@ -0,0 +1,15 @@
+package com.learn.collection.app.randomorder
+
+/**
+ * @author Rizki Rian Anandita
+ */
+fun main() {
+    val numbers = (1..10).toList()
+
+    println(numbers.shuffled())
+    println(numbers.shuffled())
+    println(numbers.shuffled())
+    println(numbers.shuffled())
+    println(numbers.shuffled())
+    println(numbers)
+}
