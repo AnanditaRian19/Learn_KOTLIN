cab1dfe25b5dec4cae427823f01736e550e51e33
AnanditaRian19 <rizkianandita6@gmail.com>
2021-01-03 10:07:01 +0700

    update OrderTest add @TestInstance

--- Kotlin_UNIT_TEST/src/test/kotlin/com/learn/OrderTest.kt
+++ Kotlin_UNIT_TEST/src/test/kotlin/com/learn/OrderTest.kt
@@ -1,32 +1,42 @@
 package com.learn
 
-import org.junit.jupiter.api.MethodOrderer
-import org.junit.jupiter.api.Order
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.TestMethodOrder
+import org.junit.jupiter.api.*
 
 /**
  * @author Rizki Rian Anandita
  */
+
+@TestInstance(value = TestInstance.Lifecycle.PER_CLASS) // Anotasi jika akan perclass, defaultnya adalah per method
 @TestMethodOrder(value = MethodOrderer.OrderAnnotation::class)
 class OrderTest {
+
+    var counter: Int = 0
 
     @Test
     @Order(3)
     fun testA() {
         println("C")
+        counter++
+        println(counter)
+        println(this.hashCode())
     }
 
     @Test
     @Order(1)
     fun testB() {
         println("A")
+        counter++
+        println(counter)
+        println(this.hashCode())
     }
 
     @Test
     @Order(2)
     fun testC() {
         println("B")
+        counter++
+        println(counter)
+        println(this.hashCode())
     }
 
 }
