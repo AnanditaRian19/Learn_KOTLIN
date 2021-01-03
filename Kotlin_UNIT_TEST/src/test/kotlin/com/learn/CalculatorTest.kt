b8064b1dedb176137e4dda665a7ac538c68daa78
AnanditaRian19 <rizkianandita6@gmail.com>
2021-01-03 09:07:42 +0700

    update Batalkan Test

--- Kotlin_UNIT_TEST/src/test/kotlin/com/learn/CalculatorTest.kt
+++ Kotlin_UNIT_TEST/src/test/kotlin/com/learn/CalculatorTest.kt
@@ -3,6 +3,7 @@ package com.learn
 import com.learn.generator.SimpleDisplayNameGenerator
 import org.junit.jupiter.api.*
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.opentest4j.TestAbortedException
 
 /**
  * @author Rizki Rian Anandita
@@ -12,12 +13,14 @@ class CalculatorTest {
 
     companion object {
 
+        // Dijalankan sekali diawal
         @BeforeAll
         @JvmStatic
         fun beforeAll() {
             println("Sebelum semua unit test")
         }
 
+        // Dijalankan sekali di akhir
         @AfterAll
         @JvmStatic
         fun afterAll() {
@@ -28,11 +31,13 @@ class CalculatorTest {
 
     val calculator = Calculator()
 
+    // Dijalankan setiap sebelum melakukan test
     @BeforeEach
     fun beforeEach() {
         println("Sebelum Unit Test")
     }
 
+    // Dijalanlkan setiap setelah test
     @AfterEach
     fun afterEach() {
         println("Setelah Unit Test")
@@ -70,6 +75,19 @@ class CalculatorTest {
     @Disabled("Sedang diperbaiki")
     fun testComingSoon() {
         // Belum selesai
+    }
+
+    // Jika value environment-nya terisi makan akan dijalankan
+    // Jika value environment-nya kosong/belum terisi makan method akan dibatalkan
+    @Test
+    fun testAborted() {
+        val profile = System.getenv()["PROFILE"]
+        if ("DEV" != profile) {
+            // Membatalkan Test
+            throw TestAbortedException()
+//            throw java.lang.IllegalArgumentException() // Maka akan di anggap error
+        }
+        println("Test Not Aborted")
     }
 
 }
