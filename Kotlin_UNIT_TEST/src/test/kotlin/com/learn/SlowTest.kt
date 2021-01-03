53b2266902c54abc8654ea5ffd3aeca9326e1289
AnanditaRian19 <rizkianandita6@gmail.com>
2021-01-03 22:50:10 +0700

    add junit-platform.properties

--- Kotlin_UNIT_TEST/src/test/kotlin/com/learn/SlowTest.kt
+++ Kotlin_UNIT_TEST/src/test/kotlin/com/learn/SlowTest.kt
@@ -2,16 +2,31 @@ package com.learn
 
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Timeout
+import org.junit.jupiter.api.parallel.Execution
+import org.junit.jupiter.api.parallel.ExecutionMode
 import java.util.concurrent.TimeUnit
 
 /**
  * @author Rizki Rian Anandita
  */
+@Execution(ExecutionMode.CONCURRENT)
 class SlowTest {
 
     @Test
     @Timeout(value = 5, unit = TimeUnit.SECONDS)
     fun testSlow() {
-        Thread.sleep(10_000)
+        Thread.sleep(3_000)
+    }
+
+    @Test
+    @Timeout(value = 5, unit = TimeUnit.SECONDS)
+    fun testSlow1() {
+        Thread.sleep(3_000)
+    }
+
+    @Test
+    @Timeout(value = 5, unit = TimeUnit.SECONDS)
+    fun testSlow2() {
+        Thread.sleep(3_000)
     }
 }
