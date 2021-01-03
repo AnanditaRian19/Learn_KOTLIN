531a1e87872fdbef42d5ace4652f39f938a76b43
AnanditaRian19 <rizkianandita6@gmail.com>
2021-01-03 11:17:28 +0700

    update add RepeatedTest in RandomCalculatorTest.kt

--- Kotlin_UNIT_TEST/src/test/kotlin/com/learn/RandomCalculatorTest.kt
+++ Kotlin_UNIT_TEST/src/test/kotlin/com/learn/RandomCalculatorTest.kt
@@ -1,7 +1,7 @@
 package com.learn
 
+import org.junit.jupiter.api.*
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Test
 import java.util.*
 
 /**
@@ -17,7 +17,25 @@ class RandomCalculatorTest : ParentCalculatorTest() {
 //    private val calculator = Calculator()
 
     @Test
-    fun testRandom(random: Random) {
+    fun testRandom(testInfo: TestInfo, random: Random) {
+        val first = random.nextInt(1000)
+        println(first)
+        val second = random.nextInt(1000)
+        println(second)
+
+        val result = calculator.add(first, second)
+
+        assertEquals(first + second, result)
+    }
+
+    @DisplayName("Test Calculator Random")
+    @RepeatedTest(
+        value = 10, // Diulang berapa kali ? 10
+        name = "{displayName} ke {currentRepetition} dari {totalRepetitions}"
+    )
+    fun testRandomRepeated(testInfo: TestInfo, random: Random, repetitionInfo: RepetitionInfo) {
+        println("==> ${testInfo.displayName} ke ${repetitionInfo.currentRepetition} dari ${repetitionInfo.totalRepetitions}")
+
         val first = random.nextInt(1000)
         println(first)
         val second = random.nextInt(1000)
