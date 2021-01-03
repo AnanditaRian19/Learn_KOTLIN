7b03c08a4f522993f46846f7937453b7b523eea2
AnanditaRian19 <rizkianandita6@gmail.com>
2021-01-03 10:14:41 +0700

    update DisplayName in InformationTest.kt

--- Kotlin_UNIT_TEST/src/test/kotlin/com/learn/InformationTest.kt
+++ Kotlin_UNIT_TEST/src/test/kotlin/com/learn/InformationTest.kt
@@ -1,7 +1,28 @@
 package com.learn
 
+import org.junit.jupiter.api.*
+
 /**
  * @author Rizki Rian Anandita
  */
+
+@DisplayName("Test with TestInfo")
 class InformationTest {
+
+    @Test
+    @Tags(
+        value = [
+            Tag("Contoh1"),
+            Tag("Contoh2")]
+    )
+
+    // Untuk mendapatkan informasi secara real time saat unit test sedang dijalankan
+    @DisplayName("Sample Test")
+    fun sampleTest(testInfo: TestInfo) {
+        println(testInfo.displayName)
+        println(testInfo.tags)
+        println(testInfo.testClass)
+        println(testInfo.testMethod)
+    }
+
 }
