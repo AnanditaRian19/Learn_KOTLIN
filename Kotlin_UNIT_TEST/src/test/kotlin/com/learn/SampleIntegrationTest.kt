71961ad94216fbf3305414a0d2a81811b27aa04a
AnanditaRian19 <rizkianandita6@gmail.com>
2021-01-03 09:50:22 +0700

    create SampleIntegrationTest

---
+++ Kotlin_UNIT_TEST/src/test/kotlin/com/learn/SampleIntegrationTest.kt
@@ -0,0 +1,21 @@
+package com.learn
+
+import org.junit.jupiter.api.Tag
+import org.junit.jupiter.api.Test
+
+/**
+ * @author Rizki Rian Anandita
+ */
+@Tag("integration-test")
+class SampleIntegrationTest {
+
+    @Test
+    fun testSample1() {
+
+    }
+
+    @Test
+    fun testSample2() {
+
+    }
+}
